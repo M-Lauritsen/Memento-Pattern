cd7bda4e674b11b6c9563b0a421598340929de9f
Michael Lauritsen <i30288741@live.dk>
2018-10-17 09:07:42 +0200

    Add project files.

---
+++ Memento Pattern/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
